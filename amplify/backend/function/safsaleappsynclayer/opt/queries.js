1436df86d7888c63da81afbcc3d1fe4219a61ca9
Claudia I. Falcon <boofalcon@MacBook-Pro-de-Claudia.local>
2021-06-17 09:47:04 -0500

    Stablish base line

--- amplify/backend/function/safsaleappsynclayer/opt/queries.js
+++ amplify/backend/function/safsaleappsynclayer/opt/queries.js
@@ -1,4 +1,7 @@
-const getConvo = /* GraphQL */ `
+const gql = require('graphql-tag');
+
+
+const getConvo = /* GraphQL */ gql`
   query GetConvo($id: ID!,$guestmail:String,$convoLinkUserId:ID!) {
     getConvo(id: $id) {
       id
